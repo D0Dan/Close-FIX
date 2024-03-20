bf3e2f3115f19dd57965be5c6ef88b36113a35f1
D0Dan <chekanov.dan@gmail.com>
2024-03-20 15:51:31 -0500

    decoding loop

--- Part1.c
+++ Part1.c
@@ -1,36 +1,76 @@
 #include<stdio.h>
 
 // structure to store FIX instruction encoding 
-typedef union Touple {
+struct pair {
 	int key;
-	char* value;
-}touple;
-
-touple Add(int key, char value[]){
-	touple New;
-	New.key = key;
-	New.value = value;
-	return New;
-}
+	char value[64];
+};
 
 void main(int argc, char** argv){
-	//Read data from Input.txt into input [max 20 charecters]
+
+	//Read data from Input.txt into input
 	char filename[] = "input.txt";
 	FILE *inputFile;
-	char input[20];
+	char input[128];
 
 	inputFile = fopen("input.txt", "r");
 	fscanf(inputFile, "%s", &input);
 	printf("Read input: %s", input);
+	fclose("input.txt");
 
 	//distinguish charecter from key-word
 	//Logon(34=56) -> Logon [key] 34=56 [value]
 	//Part1 -> assuming empty values !!!restriction!!!
 	
-	touple keyValue[5];
-	char BeginString[] = "FIX.4.2"; 
-	keyValue[1] = Add(8, BeginString);
+	//find the sum of letters in assci of mesage
+	//find start and end of content in brackets 
+	int messageType = 0;
+	int inputFlag = 0;
+	int inputFlagClose = 0;
+	for(int i = 0; input[i] =! '\0'; i ++) {
+		if (input[i] == '('){
+			inputFlag = i;
+			messageType += input[i];
+			for (int j = i; input[j] =! '\0'; j ++)
+				if (input[j] == ')')
+					inputFlagClose = j;
+			break;
+		}
+		messageType += input[i];
+	}
 
-	printf("|%d=%s|", keyValue[1].key, keyValue[1].value);
+	//split to indevidual message types
+	switch(messageType)
+	{
+		case 944:
+			printf("heartbeat");
+		case 1225:
+			printf("testrequest");
+		case 1418:
+			printf("resendrequest");
+		case 637:
+			printf("reject");
+		case 1404:
+			printf("sequencereset");
+		case 666:
+			printf("logout");
+		case 321:
+			printf("ioi");
+		case 1403:
+			printf("advertisement");
+		case 1648:
+			printf("executionreport");
+		case 1791:
+			printf("ordercancelreject");
+		case 543:
+			printf("logon");
+			if (inputFlag == 0 || inputFlagClose == 0)
+				printf("no message content");
+			else
+				printf("contents: %s", input[inputFlag::inputFlagClose] );
+		case default:
+			printf("ERORR: messaage bad :(")
+	}
+	
 	return;
 }
